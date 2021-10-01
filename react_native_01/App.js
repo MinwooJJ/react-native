0de1732362882385ab3fad6cc10ee9dda4b39448
Minwoo <minwoojung01@gmail.com>
2021-10-01 15:02:08 -0700

    Add: styles for react-native

--- react_native_01/App.js
+++ react_native_01/App.js
@@ -7,14 +7,52 @@
  */
 
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 
 function App() {
   return (
-    <View>
-      <Text>Hello World!</Text>
+    <View style={styles.mainView}>
+      <View style={styles.subView}>
+        <Text style={styles.mainText}>Hello World!</Text>
+      </View>
+      <View style={styles.subView}>
+        <Text>Hello World!</Text>
+      </View>
+      <View style={styles.anotherSubView}>
+        <Text style={styles.mainText}>Hello World!</Text>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  mainView: {
+    flex: 1,
+    backgroundColor: 'green',
+    paddingTop: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  subView: {
+    flex: 1,
+    backgroundColor: 'yellow',
+    marginBottom: 10,
+    width: '50%',
+  },
+  anotherSubView: {
+    flex: 2,
+    backgroundColor: 'yellow',
+    marginBottom: 10,
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  mainText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'red',
+    padding: 20,
+  },
+});
 
 export default App;
